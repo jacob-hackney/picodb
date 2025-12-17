94c8d462df0d0db545063fe10ee5175edd40fe43
jacob-hackney <jdhackney87@gmail.com>
2025-12-16 19:17:06 -0500

    Fix queue and read pages

--- src/internal/IOQueue.ts
+++ src/internal/IOQueue.ts
@@ -1,21 +1,38 @@
+type DBOperation<T> = () => Promise<T>;
+
 export class IOQueue {
-  #operationQueue: Promise<any>[] = [];
+  private pending: DBOperation<any>[] = [];
+  private runningCount: number = 0;
+  private isProcessing: boolean = false;
+  private isReady: boolean = false;
 
-  enqueue<T>(operation: () => Promise<T>): Promise<T> {
-    const lastOperation =
-      this.#operationQueue.length > 0
-        ? this.#operationQueue[this.#operationQueue.length - 1]
-        : Promise.resolve();
+  async enqueue<T>(operation: DBOperation<T>): Promise<T> {
+    return new Promise<T>((resolve, reject) => {
+      const task = () => operation().then(resolve).catch(reject);
+      this.pending.push(task);
 
-    const newOperation = lastOperation.then(() => operation());
-    this.#operationQueue.push(newOperation);
-
-    newOperation.finally(() => {
-      this.#operationQueue = this.#operationQueue.filter(
-        (op) => op !== newOperation
-      );
+      if (!this.isProcessing && this.isReady) this.processQueue();
     });
+  }
 
-    return newOperation;
+  private async processQueue(): Promise<void> {
+    this.isProcessing = true;
+
+    while (this.pending.length > 0 && this.runningCount < 16) {
+      for (const task of this.pending.splice(0, 16 - this.runningCount)) {
+        this.runningCount++;
+        task().finally(() => {
+          this.runningCount--;
+          if (!this.isProcessing && this.isReady) this.processQueue();
+        });
+      }
+    }
+
+    this.isProcessing = false;
+  }
+
+  start(): void {
+    this.processQueue();
+    this.isReady = true;
   }
 }
--- src/internal/StorageManager.ts
+++ src/internal/StorageManager.ts
@@ -1,21 +1,15 @@
 import { Buffer } from "node:buffer";
 import fs from "node:fs";
-import os from "node:os";
 import path from "node:path";
 
 import { IOQueue } from "./IOQueue.js";
 import envPaths from "env-paths";
 
-interface StorageManagerOptions {
-  cacheSize?: number;
-}
-const OPTIONS_DEFAULTS: StorageManagerOptions = {
-  cacheSize: 512,
-};
+interface StorageManagerOptions {}
+const OPTIONS_DEFAULTS: StorageManagerOptions = {};
 
 export class StorageManager {
   private pageSize!: number;
-  private cacheSize: number;
 
   private dirPath: string = envPaths("picodb", { suffix: "" }).data;
   private dbPath: string;
@@ -25,21 +19,12 @@ export class StorageManager {
   private lockHandle!: fs.promises.FileHandle;
   private logHandle!: fs.promises.FileHandle;
 
-  private operationQueue: IOQueue = new IOQueue();
+  private queue: IOQueue = new IOQueue();
 
   private cache: Map<number, Buffer> = new Map();
 
   constructor(options?: StorageManagerOptions) {
     const finalOptions = { ...OPTIONS_DEFAULTS, ...options };
-    if (
-      finalOptions.cacheSize! <= 0 ||
-      !Number.isInteger(finalOptions.cacheSize!)
-    ) {
-      throw new RangeError(
-        "Invalid cacheSize option. It must be a positive integer."
-      );
-    }
-    this.cacheSize = finalOptions.cacheSize!;
 
     this.dbPath = path.join(this.dirPath, "pico.db");
     this.lockPath = path.join(this.dirPath, "picodb.lock");
@@ -49,7 +34,7 @@ export class StorageManager {
     });
   }
 
-   private async init() {
+  private async init() {
     await fs.promises.access(this.dbPath).catch((err) => {
       throw new Error(
         `StorageManager initialization failed: ${err.message}${
@@ -66,37 +51,39 @@ export class StorageManager {
         path.join(this.dirPath, "picodb.binlog"),
         "a+"
       );
+      this.pageSize = (
+        await this.dbHandle.read(Buffer.alloc(8), 0, 4, 0)
+      ).buffer.readUInt32LE(0);
+      this.queue.start();
     } catch {
       throw new Error(
-        `StorageManager failed to access files in ${
-          this.dirPath
-        }. Try running the process with elevated permissions(sudo).`
+        `StorageManager failed to access files in ${this.dirPath}. Try running the process with elevated permissions(sudo).`
       );
     }
   }
 
   async allocatePage(): Promise<number> {
-    const callerPromise = new Promise<number>(async (resolve, reject) => {
-      const executionLogic = async () => {
-        try {
-          const stats = await this.dbHandle.stat();
-          const pageIndex = Math.floor(stats.size / this.pageSize);
-          const buffer = Buffer.alloc(this.pageSize);
-          await this.dbHandle.write(buffer, 0, this.pageSize, stats.size);
+    const executionLogic = async (): Promise<number> => {
+      const stats = await this.dbHandle.stat();
+      const pageIndex = Math.floor(stats.size / this.pageSize);
+      const buffer = Buffer.alloc(this.pageSize);
+      await this.dbHandle.write(buffer, 0, this.pageSize, stats.size);
+      return pageIndex;
+    };
 
-          resolve(pageIndex);
-        } catch (err) {
-          reject(err);
-        }
-      };
-
-      this.operationQueue.enqueue(executionLogic);
-    });
-
-    return callerPromise;
+    return this.queue.enqueue(executionLogic);
   }
 
-  //async readPage(pageIndex: number): Promise<Buffer> {}
+  async readPage(pageIndex: number): Promise<Buffer> {
+    const executionLogic = async (): Promise<Buffer> => {
+      const buffer = Buffer.alloc(this.pageSize);
+      const position = pageIndex * this.pageSize + 4;
+      return (await this.dbHandle.read(buffer, 0, this.pageSize, position))
+        .buffer;
+    };
+
+    return this.queue.enqueue(executionLogic);
+  }
   //async writePage(pageIndex: number, data: Buffer): Promise<void> {}
 
   static async create(
@@ -122,16 +109,16 @@ export class StorageManager {
       );
     }
     await fs.promises.rm(dirPath, { recursive: true, force: true });
-
     const dbPath = path.join(dirPath, "pico.db");
     await fs.promises.mkdir(dirPath, { recursive: true });
     const handle = await fs.promises.open(dbPath, "w+");
     await fs.promises.open(path.join(dirPath, "picodb.lock"), "w+");
     await fs.promises.open(path.join(dirPath, "picodb.binlog"), "w+");
     const pageSize = pageSizeKB * 1024;
-    const initialBuffer = Buffer.alloc(pageSize);
+    const initialBuffer = Buffer.alloc(4);
     initialBuffer.writeUInt32LE(pageSize, 0);
-    await handle.write(initialBuffer, 0, pageSize, 0);
+    await handle.write(initialBuffer, 0, 4, 0);
+    await handle.write(Buffer.alloc(pageSize), 0, pageSize, 4);
     await handle.close();
   }
 }
